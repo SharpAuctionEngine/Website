31125edf382e058df3d76b5dd267b5d8a5044ced
karthick92 <karthick@sae.bid>
2016-03-13 03:42:28 -0500

    #29,AJAX and POST cal has been added

---
+++ public/js/contact.js
@@ -0,0 +1,27 @@
+jQuery(function($)  
+{
+	var $data=$('#contact-form')
+
+        $.ajax(
+        {
+            type: "POST",
+            url: "contact-us/submit",
+            data: $data.serialize(),
+         success: function()
+         {
+         	bootbox.alert('Your message has been sent.Thank you!');
+         	$("#name").val(''); // reset field after successful submission
+            $("#email").val(''); // reset field after successful submission
+            $("#msg").val(''); // reset field after successful submission
+            $("#phoneNumber").val('');
+            $("#subject").val('');
+         },
+         error:function()
+         {
+         	bootbox.alert('There was an error in sending message. Please try again!');
+
+         },  
+        })
+
+
+});
