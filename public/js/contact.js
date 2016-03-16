f695ff07f491f3d3b50f3fed2f1bad5309f7ecdc
karthick92 <karthick@sae.bid>
2016-03-16 13:04:16 -0500

    #29,email validation added

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,7 +1,12 @@
 jQuery(function($)  
-{
+{ 
+	$("#contact_form").submit(function()
+    {
 	var $data=$('#contact-form')
-
+	var email=$("#email").val();
+	console.log('true');
+	if( isValidEmailAddress( email ) )
+        {
         $.ajax(
         {
             type: "POST",
@@ -17,11 +22,22 @@ jQuery(function($)
             $("#subject").val('');
          },
          error:function()
-         {
-         	bootbox.alert('There was an error in sending message. Please try again!');
+         {   
 
+         	bootbox.alert('There was an error in sending message. Please try again!');
+         	
          },  
         });
-
-
+     }
+     else
+     {		
+           bootbox.alert('Invalid Email Address');
+           
+     }
+     return false;// prevent page refresh
+    });
 });
+	function isValidEmailAddress(emailAddress) {
+    var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+    return pattern.test(emailAddress);
+    };
