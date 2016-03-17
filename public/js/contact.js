b2bd16cfa22e7bec1b7f81fcdba11287838434ae
InstanceOfMichael <michael@spacegazebo.com>
2016-03-17 17:29:42 -0500

    debugging contactus form

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,43 +1,36 @@
-jQuery(function($)  
-{ 
-	$("#contact_form").submit(function()
-    {
-	var $data=$('#contact-form')
-	var email=$("#email").val();
-	console.log('true');
-	if( isValidEmailAddress( email ) )
-        {
-        $.ajax(
-        {
-            type: "POST",
-            url: "/contact-us/submit",
-            data: $data.serialize(),
-         success: function()
-         {
-         	bootbox.alert('Your message has been sent.Thank you!');
-         	$("#name").val(''); // reset field after successful submission
-            $("#email").val(''); // reset field after successful submission
-            $("#msg").val(''); // reset field after successful submission
-            $("#phoneNumber").val('');
-            $("#subject").val('');
-         },
-         error:function()
-         {   
+jQuery(function($) {
+    $('body').on('submit','#contact_form',function() {
+        var $form = $('#contact-form');
+        var email = $("#email").val();
+        console.log('true');
+        if (isValidEmailAddress(email)) {
+            $.ajax({
+                method: "POST",
+                url: "/contact-us/submit",
+                data: $form.serialize(),
+                success: function() {
+                    bootbox.alert('Your message has been sent.Thank you!');
+                    $("#name").val(''); // reset field after successful submission
+                    $("#email").val(''); // reset field after successful submission
+                    $("#msg").val(''); // reset field after successful submission
+                    $("#phoneNumber").val('');
+                    $("#subject").val('');
+                },
+                error: function() {
 
-         	bootbox.alert('There was an error in sending message. Please try again!');
-         	
-         },  
-        });
-     }
-     else
-     {		
-           bootbox.alert('Invalid Email Address');
-           
-     }
-     return false;// prevent page refresh
+                    bootbox.alert('There was an error in sending message. Please try again!');
+
+                },
+            });
+        } else {
+            bootbox.alert('Invalid Email Address');
+
+        }
+        return false; // prevent page refresh
     });
 });
-	function isValidEmailAddress(emailAddress) {
+
+function isValidEmailAddress(emailAddress) {
     var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
     return pattern.test(emailAddress);
-    };
+};
