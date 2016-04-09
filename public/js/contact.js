ae4fa768a09e970b94e374e8aeef1adba8a195aa
karthick92 <karthick@sae.bid>
2016-04-09 00:21:32 -0500

    cleanups

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,10 +1,9 @@
 function isValidEmailAddress(emailAddress) {
     var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
     return pattern.test(emailAddress);
-};
+}
 
-function resetContactUsForm ()
-{
+function resetContactUsForm() {
     $("#name").val(''); // reset field after successful submission
     $("#email").val(''); // reset field after successful submission
     $("#msg").val(''); // reset field after successful submission
@@ -12,54 +11,53 @@ function resetContactUsForm ()
     $("#subject").val(0);
 }
 
-$(function($) {
-    $('body').on('submit','#contact_form',function() {
+$(function ($) {
+    $('body').on('submit', '#contact_form', function () {
         var $form = $('#contact_form');
         var email = $("#email").val();
 
         console.log({
-            '$form':$form,
-            '$form.serialize()':$form.serialize(),
+            '$form': $form,
+            '$form.serialize()': $form.serialize()
         });
 
         // if (isValidEmailAddress(email)) {
-            $.ajax({
-                method: "POST",
-                url: "/contact-us/submit",
-                data: $form.serialize(),
-                success: function() {
-                    $(' #messagebag ').remove();
-                    // fg.removeClass('has-error');
-                    $('.form-group').removeClass('has-error');
+        $.ajax({
+            method: "POST",
+            url: "/contact-us/submit",
+            data: $form.serialize(),
+            success: function () {
+                $(' #messagebag ').remove();
+                // fg.removeClass('has-error');
+                $('.form-group').removeClass('has-error');
 
-                    bootbox.alert('Your message has been sent.Thank you!');
-                    resetContactUsForm();
-                },
-                error: function(xhr, textStatus, errorThrown) {
-                    $(' #messagebag ').remove();
-                    
-                     console.error({
+                bootbox.alert('Your message has been sent.Thank you!');
+                resetContactUsForm();
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                $(' #messagebag ').remove();
+
+                console.error({
                     textStatus: textStatus,
                     'xhr.response': xhr.responseJSON || xhr.responseText,
-                    errorThrown:errorThrown,
+                    errorThrown: errorThrown
                 });
-                var json= xhr.responseJSON || xhr.responseText ||{};
-                     var alerts =new MessageBag();
-                       if(xhr.responseJSON.errors)
-                    {
+                var json = xhr.responseJSON || xhr.responseText || {};
+                var alerts = new MessageBag();
+                if (xhr.responseJSON.errors) {
 
-                    jQuery.each(xhr.responseJSON.errors, function(key, value) {
-                     alerts.add(value.field,value.message);
+                    jQuery.each(xhr.responseJSON.errors, function (key, value) {
+                        alerts.add(value.field, value.message);
 
                     });
-                     alerts.sprinkle('form:first');
+                    alerts.sprinkle('form:first');
                     bootbox.alert('There was an error in sending message. Please try again!');
 
-                 }
-                },
-            });
+                }
+            }
+        });
         // } else {
-            // bootbox.alert('Invalid Email Address');
+        // bootbox.alert('Invalid Email Address');
 
         // }
         return false; // prevent page refresh
--- public/js/templatemo_script.js
+++ public/js/templatemo_script.js
@@ -1,12 +1,12 @@
 var top_menu_height = 0;
-jQuery(function($) {
-		$(window).load( function() {
-			$('.external-link').unbind('click');	
-		});
-		
-        $(document).ready( function() {
+jQuery(function ($) {
+    $(window).load(function () {
+        $('.external-link').unbind('click');
+    });
 
-            // load google map
+    $(document).ready(function () {
+
+        // load google map
         var script = document.createElement('script');
         script.type = 'text/javascript';
         script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
@@ -15,21 +15,24 @@ jQuery(function($) {
 
         top_menu_height = $('.top-menu').height();
         // scroll spy to auto active the nav item
-        $('body').scrollspy({ target: '#templatemo-nav-bar', offset: top_menu_height + 10 });
-		$('.external-link').unbind('click');
+        $('body').scrollspy({
+            target: '#templatemo-nav-bar',
+            offset: top_menu_height + 10
+        });
+        $('.external-link').unbind('click');
 
         // scroll to top
-        $('#btn-back-to-top').click(function(e){
+        $('#btn-back-to-top').click(function (e) {
             e.preventDefault();
             scrollTo('#templatemo-top');
         });
 
         // scroll to specific id when click on menu
-        $('.top-menu .navbar-nav a').click(function(e){
-            e.preventDefault(); 
+        $('.top-menu .navbar-nav a').click(function (e) {
+            e.preventDefault();
             var linkId = $(this).attr('href');
             scrollTo(linkId);
-            if($('.navbar-toggle').is(":visible") === true){
+            if ($('.navbar-toggle').is(":visible") === true) {
                 $('.navbar-collapse').collapse('toggle');
             }
             $(this).blur();
@@ -37,29 +40,29 @@ jQuery(function($) {
         });
 
         // to stick navbar on top
-       // $('.top-menu ').stickUp();
+        // $('.top-menu ').stickUp();
 
 
         // gallery category
-        $('.templatemo-gallery-category a').click(function(e){
-            e.preventDefault(); 
+        $('.templatemo-gallery-category a').click(function (e) {
+            e.preventDefault();
             $(this).parent().children('a').removeClass('active');
             $(this).addClass('active');
             var linkClass = $(this).attr('href');
-            $('.gallery').each(function(){
-                if($(this).is(":visible") === true){
-                   $(this).hide();
+            $('.gallery').each(function () {
+                if ($(this).is(":visible") === true) {
+                    $(this).hide();
                 }
             });
-            $(linkClass).fadeIn();  
+            $(linkClass).fadeIn();
         });
 
         //gallery light box setup
         $('a.colorbox').colorbox({
-                                    rel: function(){
-                                        return $(this).data('group');
+            rel: function () {
+                return $(this).data('group');
 
-                                    }
+            }
         });
 
 
@@ -70,56 +73,57 @@ function initialize() {
 
 
 
-        var _801_franklin=new google.maps.LatLng(34.722744, -86.578332);
+    var _801_franklin = new google.maps.LatLng(34.722744, -86.578332);
 
-        var mapOptions = {
-          center:_801_franklin,
-          zoom:14,
-          mapTypeId:google.maps.MapTypeId.ROADMAP
-          };
+    var mapOptions = {
+        center: _801_franklin,
+        zoom: 14,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
 
-        var map=new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-        var marker=new google.maps.Marker({
-          position:_801_franklin,
-          });
+    var marker = new google.maps.Marker({
+        position: _801_franklin
+    });
 
-        marker.setMap(map);
+    marker.setMap(map);
 
 }
 
 // scroll animation 
-function scrollTo(selectors)
-{
+function scrollTo(selectors) {
 
-    if(!$(selectors).size()) return;
+    if (!$(selectors).size()) return;
     var selector_top = $(selectors).offset().top - top_menu_height;
-    $('html,body').animate({ scrollTop: selector_top }, 'slow');
+    $('html,body').animate({
+        scrollTop: selector_top
+    }, 'slow');
 
 }
 
 $("#Carousel").carousel({
     pause: true,
     interval: 2000,
-  });
+});
 
 //Set the carousel options
-  $('#quote-carousel').carousel({
+$('#quote-carousel').carousel({
     pause: true,
     interval: 10000,
-  });
-  
- //Set the carousel options
-  $('#timeline-carousel').carousel({
+});
+
+//Set the carousel options
+$('#timeline-carousel').carousel({
     pause: true,
     interval: 10000,
-  });
+});
 
 //resize event for timeline
-  $( window ).resize(function() {
-            if(window.innerWidth > 600){
-            $('.events-content').css('height',246+'px');
-            $('.events').css('transform','translateX(-'+480+'px)');
+$(window).resize(function () {
+    if (window.innerWidth > 600) {
+        $('.events-content').css('height', 246 + 'px');
+        $('.events').css('transform', 'translateX(-' + 480 + 'px)');
 
-            }
-        });
+    }
+});
